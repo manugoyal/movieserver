fb72968cf739057bcdfa487045463c1c8ed46c4d
Manu Goyal <manu.goyal2013@gmail.com>
2013-08-05 16:43:30 -0400

    Set up a css and js structure. Styled it up a bit. Refactored the server code a little.

---
+++ js/startup.js
@@ -0,0 +1,6 @@
+requirejs.config({
+});
+
+requirejs([], function() {
+  return;
+});
--- server.go
+++ server.go
@@ -9,9 +9,9 @@ import (
         "path/filepath"
         "os"
         "flag"
-	"io"
-	"time"
-	"strings"
+        "io"
+        "time"
+        "strings"
 )
 
 
@@ -20,20 +20,23 @@ const (
         port = ":8080"
 )
 var (
-        moviePath = flag.String("movie-path", "/main/movies", "The path of the movies directory")
+        moviePath = flag.String("movie-path", "movies", "The path of the movies directory")
         movieNames []string
-	allowedIP = map[string]bool{"[::1]": true, "98.236.150.191": true, "174.51.196.185": true}
+        allowedIP = map[string]bool{"[::1]": true, "98.236.150.191": true, "174.51.196.185": true}
 )
 
 // Walks through the moviePath directory and appends any movie file
-// names to movieNames
+// names to movieNames, styling the movie name correctly
 func movieWalkFn(path string, info os.FileInfo, err error) error {
         if err != nil {
                 log.Fatal(err)
         }
-        if name := info.Name(); !info.IsDir() && name[0] != '.' &&
-                filepath.Ext(name) != ".srt" {
-                movieNames = append(movieNames, path[len(*moviePath):])
+        if name := info.Name(); !info.IsDir() && name[0] != '.' {
+		ext := filepath.Ext(path)
+		unstyled := path[len(*moviePath):len(path)-len(ext)]
+		words := strings.Split(unstyled, "_")
+		styled := strings.Title(strings.Join(words, " "))
+                movieNames = append(movieNames, styled + ext)
         }
         return nil
 }
@@ -79,63 +82,64 @@ func runTemplate(operationName string, w http.ResponseWriter, data interface{}) 
 const (
         imagePath = "images/"
         mainPath = "/"
-	fetchPath = "/fetch/"
+        fetchPath = "/fetch/"
 )
 
 // Makes sure that the request's ip is allowed. Sends an error message
 // if it isn't. Returns true if it is allowed, false if it isn't
 func checkAccess(w http.ResponseWriter, r *http.Request) bool {
-	ipstr := r.RemoteAddr[:strings.LastIndex(r.RemoteAddr, ":")]
-	if _, ok := allowedIP[ipstr]; !ok {
-		http.Error(w, fmt.Sprint("You do not have access to this site"), http.StatusServiceUnavailable)
-		return false
-	}
-	return true
-}
-
-// Reindexes the movie directory and serves index template with the
-// movie names
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	if !checkAccess(w, r) {
-		return
-	}
-        log.Print("Indexing movie directory")
-	movieNames = make([]string, 0)
-        err := filepath.Walk(*moviePath, movieWalkFn)
-        if err != nil {
-                log.Fatal(err)
+        ipstr := r.RemoteAddr[:strings.LastIndex(r.RemoteAddr, ":")]
+        if _, ok := allowedIP[ipstr]; !ok {
+                http.Error(w, fmt.Sprint("You do not have access to this site"), http.StatusServiceUnavailable)
+                return false
         }
-        runTemplate("index", w, movieNames)
+        return true
+}
+// If the URL is empty (just "/"), then it reindexes the movie
+// directory and serves index template with the movie names.
+// Otherwise, it serves the file named by the path
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+        if !checkAccess(w, r) {
+                return
+        }
+        if len(r.URL.Path) == 1 {
+                log.Print("Indexing movie directory")
+                movieNames = make([]string, 0)
+                err := filepath.Walk(*moviePath, movieWalkFn)
+                if err != nil {
+                        log.Fatal(err)
+                }
+                runTemplate("index", w, movieNames)
+        } else {
+                http.ServeFile(w, r, r.URL.Path[1:])
+        }
 }
 
-// Serves the favicon
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
-	if !checkAccess(w, r) {
-		return
-	}
-        http.ServeFile(w, r, imagePath + "favicon.ico")
-}
-
-// Serves the specified file moviePath should not be in the url
+// Serves the specified file. *moviePath should not be in the url
 func fetchHandler(w http.ResponseWriter, r *http.Request) {
-	if !checkAccess(w, r) {
-		return
-	}
-	filename := r.URL.Path[len(fetchPath):]
-	log.Printf("Fetching file: %s", *moviePath + filename)
-	f, err := os.Open(*moviePath + filename)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Could not fetch file %s", filename), http.StatusNotFound)
-		return
-	}
-	rs := io.ReadSeeker(f)
-	http.ServeContent(w, r, filename, time.Time{}, rs)
-	log.Printf("Served file: %s", *moviePath + filename)
+        if !checkAccess(w, r) {
+                return
+        }
+
+        filename := r.URL.Path[len(fetchPath):]
+        filelocation := *moviePath + filename
+        log.Printf("Fetching file: %s", filelocation)
+        f, err := os.Open(filelocation)
+        if err != nil {
+                http.Error(w, fmt.Sprintf("Could not serve file %s", filename), http.StatusNotFound)
+                return
+        }
+	defer f.Close()
+
+        rs := io.ReadSeeker(f)
+        w.Header().Set("Content-Type", "binary/octet-stream")
+        http.ServeContent(w, r, filename, time.Time{}, rs)
+        log.Printf("Served file: %s", *moviePath + filename)
 }
 
 func main() {
         flag.Parse()
-	*moviePath = filepath.Clean(*moviePath) + "/"
+        *moviePath = filepath.Clean(*moviePath) + "/"
         log.Print("Fetching html templates")
         err := fetchTemplates("index")
         if err != nil {
@@ -143,8 +147,7 @@ func main() {
         }
 
         http.HandleFunc(mainPath, mainHandler)
-        http.HandleFunc("/favicon.ico", faviconHandler)
-	http.HandleFunc(fetchPath, fetchHandler)
+        http.HandleFunc(fetchPath, fetchHandler)
 
         log.Printf("Listening on port %s\n", port[1:])
         http.ListenAndServe(port, nil)
